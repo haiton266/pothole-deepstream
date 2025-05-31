13c1b6d0d2869f98621236b14f230955eb1185e1
Hai Ton <haikesuki@gmail.com>
2025-05-31 11:24:06 +0700

    feat: add code check gps each second and collect video

---
+++ check_gps.sh
@@ -0,0 +1,1 @@
+sudo python3 /home/jetson/hai/app_copy/_my-app/server_local/gps_module/python3/loop.py
---
+++ collect_video.sh
@@ -0,0 +1,14 @@
+FILENAME="output_$(date +%Y%m%d_%H%M%S).mp4"
+
+gst-launch-1.0 -e v4l2src device=/dev/video0 ! \
+  image/jpeg, width=1280, height=720, framerate=25/1 ! \
+  jpegdec ! \
+  nvvideoconvert ! \
+  nvv4l2h264enc ! \
+  h264parse ! \
+  qtmux ! \
+  filesink location=$FILENAME
+
+# gst-launch-1.0 -e filesrc location=/home/jetson/hai/app_copy/_my-app/videos/input/dashcam.mp4 ! \
+# qtdemux name=demux demux.video_0 ! queue ! h264parse ! avdec_h264 ! videoconvert ! x264enc ! mp4mux ! \
+# filesink location=output.mp4
